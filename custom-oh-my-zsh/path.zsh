8c301da58d0375cc55218ee67018619a33d578df
Jared Stilwell <jared.k.stilwell@gmail.com>
2015-08-03 11:45:28 -0500

    No need to set an explicit version of node

--- custom-oh-my-zsh/path.zsh
+++ custom-oh-my-zsh/path.zsh
@@ -6,7 +6,6 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting¬
 
 export NVM_DIR="/Users/jstilwell/.nvm"¬
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm use 0.10 > /dev/null
 
 # NPM
 export PATH=$PATH:/usr/local/share/npm/bin
