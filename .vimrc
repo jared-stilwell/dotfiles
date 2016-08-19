5772249c717dd398ab03a11792faa3804acb24bb
Jared Stilwell <jstilwell@rmn.com>
2016-08-19 12:58:11 -0500

    Migrated to vim-plug for plugin management

--- .vimrc
+++ .vimrc
@@ -1,6 +1,9 @@
 " Include the base vim configuration
 source ~/.vimrc-base
 
+" Plugin manager
+source ~/.vimrc-plug
+
 " NERDTree configuration
 source ~/.vimrc-nerdtree
 
--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ install $DIR/custom-oh-my-zsh ~/.oh-my-zsh/custom
 # Vim dotfiles
 install $DIR/.vimrc ~/.vimrc
 install $DIR/.vimrc-base ~/.vimrc-base
+install $DIR/.vimrc-plug ~/.vimrc-plug
 install $DIR/.vimrc-nerdtree ~/.vimrc-nerdtree
 install $DIR/.vimrc-syntastic ~/.vimrc-syntastic
 install $DIR/.vimrc-airline ~/.vimrc-airline
@@ -36,23 +37,14 @@ mkdir -p $HOME/.config
 ln -fs ~/.vim $HOME/.config/nvim
 ln -fs ~/.vimrc $HOME/.config/nvim/init.vim
 ln -fs ~/.vimrc-base $HOME/.config/nvim/.vimrc-base
-ln -fs ~/.vimrc-airline $HOME/.config/nvim/.vimrc-airline
+# ln -fs ~/.vimrc-airline $HOME/.config/nvim/.vimrc-airline
 ln -fs ~/.vimrc-nerdtree $HOME/.config/nvim/.vimrc-nerdtree
 ln -fs ~/.vimrc-syntastic $HOME/.config/nvim/.vimrc-syntastic
 
-## Pathogen
+## vim-plug plugin manager
 rm -rf ~/.vim/autoload
 mkdir -p ~/.vim/autoload
-install $DIR/link/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
-
-## Vim Plugins
-install_vim_plugin nerdtree
-install_vim_plugin syntastic
-install_vim_plugin vim-airline
-install_vim_plugin vim-airline-themes
-install_vim_plugin vim-polyglot
-install_vim_plugin editorconfig-vim
-install_vim_plugin scss-syntax
+install $DIR/link/vim-plug/plug.vim ~/.vim/autoload/plug.vim
 
 # Editor Config
 install $DIR/.editorconfig ~/.editorconfig
